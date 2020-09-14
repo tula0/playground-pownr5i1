22049bdd14dd070f79b735a3789ddf61a1782eeb
Lukio <lukio.olweny@gmail.com>
2020-09-14 11:29:45 +0300

    Update app.module.ts

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import * as echarts from 'echarts';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { NgxechartsDemoComponent } from './ngxecharts-demo/ngxecharts-demo.component';
+
+// services
+import {CovidHttpServiceService  } from './covid-http-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent, NgxechartsDemoComponent,CovidHttpServiceService],
   imports: [
     BrowserModule,
+    NgxEchartsModule.forRoot({
+      echarts
+    }),
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ CovidHttpServiceService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
